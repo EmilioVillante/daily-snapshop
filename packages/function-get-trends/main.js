4c436493b65de9c7e678ea1a1e312d9ccf5f5dd0
emiliovillante <e.villante.griffith@gmail.com>
2022-12-23 16:58:22 +1100

    Adds date query param for finding trends. Limited by Google only providing one month of data.

--- packages/function-get-trends/main.js
+++ packages/function-get-trends/main.js
@@ -3,12 +3,14 @@
 const functions = require('@google-cloud/functions-framework');
 const escapeHtml = require('escape-html');
 
-const TREND_API = 'https://trends.google.com/trends/api/topdailytrends';
+const TREND_API = 'https://trends.google.com/trends/api/dailytrends';
 
 /**
  * Call Google trends api and return the list of daily trends titles
  *
- * @param req.query.geo (String) Geo code for localisation of trends
+ * @param {String} req.query.geo (geo=AU) Geo code for localisation of trends
+ * @param {String} req.query.date (date=2022-12-04) Date to query trends for. Google only provides the last month from today.
+ *
  * @returns Array Trends relevant to the given day
  */
 functions.http('getTrends', async (req, res) => {
@@ -20,19 +22,34 @@ functions.http('getTrends', async (req, res) => {
         console.log('CONFIG', JSON.stringify(CONFIG));
 
         const geo = req.query.geo ? escapeHtml(req.query.geo) : CONFIG.DEFAULT_GEO;
-        const url = `${TREND_API}?${new URLSearchParams({geo})}`;
 
-        console.log('trend fetch URL', url);
+        let queryParams = {
+            geo,
+            hl: 'en-US',
+            ...(req.query.date && {ed: escapeHtml(req.query.date).replaceAll('-', '')})
+        }
+
+        console.log('QUERY PARAMS', JSON.stringify(queryParams));
+
+        const url = `${TREND_API}?${new URLSearchParams(queryParams)}`;
+
+        console.log('TREND FETCH URL', url);
 
         const response = await fetch(url, {method: 'GET'});
         const responseStr = await response.text();
 
-        console.log('response', responseStr);
+        console.log('TREND RESPONSE', responseStr);
 
         const data = JSON.parse(responseStr.replace(')]}\',', ''));
-        const trends = data.default.trendingSearches.map((trend) => trend.title);
 
-        console.log('trends', trends)
+        // End if there are no trends
+        if (!data.default.trendingSearchesDays.length) {
+            throw new Error('No trends for the given day')
+        }
+
+        const trends = data.default.trendingSearchesDays[0].trendingSearches.map((trend) => trend.title.query);
+
+        console.log('TRENDS', trends)
 
         res.send(trends)
     } catch(e) {
